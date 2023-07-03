65f894fae3fd0199a7e9c85e9cd1e63a89c60530
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-07-03 23:43:40 +0530

    middleware error handled successfully

--- controllers/who_i_am.js
+++ controllers/who_i_am.js
@@ -2,33 +2,40 @@ import jwt from "jsonwebtoken";
 import { Users } from "../models/users.js";
 
 export const who_i_am = async (req, res, next) => {
-    const { token } = req.cookies
+    const { token } = req.cookies;
 
     if (!token) {
-        return res.redirect("/");
+        return res.redirect("/loginfirst");
     }
 
-    const decode = jwt.verify(token, process.env.JWT_SECRET)
-
-    if (!decode) {
-        return res.redirect("/");
-    }
     try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET);
         req.user = await Users.findById(decode._id);
+
         switch (req.user.adminType) {
-            case "admin": res.redirect("/admin")
+            case "admin":
+                res.redirect("/admin");
                 break;
-            case "accountant": res.redirect("/accounts")
+            case "accountant":
+                res.redirect("/accounts");
                 break;
-            case "delivery": res.redirect("/delivery")
+            case "delivery":
+                res.redirect("/delivery");
                 break;
-            case "customer": res.redirect("/")
+            case "customer":
+                res.redirect("/customer");
                 break;
-            default: res.redirect("/login")
+            default:
+                res.redirect("/login");
                 break;
         }
     } catch (error) {
-        return res.redirect("/");
+        if (error.name === "JsonWebTokenError") {
+            return res.redirect("/decodefailed");
+        } else {
+            return res.redirect("/");
+        }
     }
-    next()
-}
+
+    next();
+};
