66d409c8008eef223c8ac3381c8effaeae9d6c89
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-18 20:04:33 +0530

    schema change

--- models/products.js
+++ models/products.js
@@ -1,121 +1,85 @@
 const mongoose = require("mongoose");
 
-const products = new mongoose.Schema({
+const products = new mongoose.Schema(
+  {
     productName: {
-        type: String,
-        required: true
-    },
-    sellerID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Users",
-        required: true
+      type: String,
+      required: true,
     },
     productType: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     shortDescription: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     tags: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
     tax: {
-        type: Number,
-        required: true
-    },
-    indicator: {
-        type: String,
-        required: true,
+      type: Number,
+      default: 12,
     },
     madeIn: {
-        type: String,
-        default: "India"
+      type: String,
+      default: "India",
     },
     brand: {
-        type: String,
-        required: true
-    },
-    totalAllowedQuantity: {
-        type: Number,
-        default: 5
-    },
-    minOrderQuantity: {
-        type: Number,
-        default: 1
-    },
-    qualityStepSize: {
-        type: String,
-        required: true
-    },
-    warrantyPeriod: {
-        type: String,
-    },
-    guaranteePeriod: {
-        type: String,
-    },
-    deliverableType: {
-        type: [String],
-        required: true
-    },
-    deliverableZipcodes: {
-        type: [Number],
-        required: true
+      type: String,
+      default: "Augse",
     },
     hsnCode: {
-        type: String,
-    },
-    taxIncludedPrice: {
-        type: Number,
-
+      type: String,
     },
     isCodAllowed: {
-        type: Boolean,
-        required: true
+      type: Boolean,
+      required: true,
     },
     isReturnAble: {
-        type: Boolean,
-        required: true
+      type: Boolean,
+      required: true,
     },
     isCancelAble: {
-        type: Boolean,
-        required: true
+      type: Boolean,
+      required: true,
     },
     mrp: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
-    currentPrice: {
-        type: Number,
-        required: true
+    offeredPrice: {
+      type: Number,
+      required: true,
     },
     mainImage: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     otherImage: {
-        type: [String],
-        required: true
+      type: [String],
+      required: true,
     },
     productVideo: {
-        type: String,
+      type: String,
     },
-    description: {
-        type: String,
-        required: true
+    DetailedDescription: {
+      type: String,
+      required: true,
     },
     visibility: {
-        type: String,
-        default: false
+      type: String,
+      default: true,
     },
     viewCount: {
-        type: Number,
-        default: 0
-    }
-}, {
-    timestamps: true
-});
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 exports.Products = mongoose.model("Products", products);
