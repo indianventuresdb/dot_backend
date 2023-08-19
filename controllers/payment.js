c022f531e1a35e9ef29ac7b00356c7687b08cf00
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-19 15:37:32 +0530

    error fixed

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,32 +1,24 @@
 const Razorpay = require("razorpay");
 
 var instance = new Razorpay({
-  key_id: process.env.RAZORPAY_API_KEY,
+  key_id: `${process.env.RAZORPAY_API_KEY}`,
   key_secret: process.env.RAZORPAY_API_SECRET,
 });
 
 exports.checkOut = async (req, res) => {
   try {
-    console.log(process.env.RAZORPAY_API_KEY, process.env.RAZORPAY_API_SECRET);
-
     const options = {
       amount: 5000,
       currency: "INR",
       receipt: "randomNumber",
     };
-    const order = instance.orders.create(options, (err, order) => {
-      console.log(order);
-    });
-
+    const order = await instance.orders.create(options);
+    order ? console.log(order) : null;
     res.status(200).json({ success: true, order });
   } catch (error) {
     console.error("Error creating order:", error);
-    res.status(500).json({
+    res.status(401).json({
       success: false,
-      error: {
-        key: process.env.RAZORPAY_API_KEY,
-        secret: process.env.RAZORPAY_API_SECRET,
-      },
     });
   }
 };
