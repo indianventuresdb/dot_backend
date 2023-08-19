c4c26291e4a90b7d7205a7b0cf9eae7dc336cb3d
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2023-08-19 14:53:01 +0530

    pushibng with error

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,20 +1,32 @@
-const { instance } = require("../server");
+const Razorpay = require("razorpay");
+
+var instance = new Razorpay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
 
 exports.checkOut = async (req, res) => {
   try {
-    console.log(instance);
+    console.log(process.env.RAZORPAY_API_KEY, process.env.RAZORPAY_API_SECRET);
 
     const options = {
       amount: 5000,
       currency: "INR",
       receipt: "randomNumber",
     };
-    const order = await instance.orders.create(options);
-    console.log(order);
+    const order = instance.orders.create(options, (err, order) => {
+      console.log(order);
+    });
 
-    res.status(200).json({ success: true });
+    res.status(200).json({ success: true, order });
   } catch (error) {
     console.error("Error creating order:", error);
-    res.status(500).json({ success: false, error: "Error creating order" });
+    res.status(500).json({
+      success: false,
+      error: {
+        key: process.env.RAZORPAY_API_KEY,
+        secret: process.env.RAZORPAY_API_SECRET,
+      },
+    });
   }
 };
--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const { app } = require("./app.js");
 const connectDb = require("./database/connectDb.js");
-const Razorpay = require("razorpay");
 
 connectDb();
 
-const instance = new Razorpay({
-  key_id: process.env.RAZORPAY_API_KEY,
-  key_secret: process.env.RAZORPAY_API_SECRET,
-});
-
-exports.instance = instance; // Export the instance directly
 
 app.listen(process.env.PORT, function () {
   console.log("server Started on http://localhost:" + process.env.PORT);
