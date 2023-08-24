10c86cee3d465f87685ee6d11c3daccf9e3c1792
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-08-24 22:07:11 +0530

    ImagesOfNewProducts

--- routes/products.js
+++ routes/products.js
@@ -8,6 +8,8 @@ const {
   getProducts,
   searchProducts,
   getOneProduct,
+  addImage,
+  deleteImage,
   productNumbers,
 } = require("../controllers/products.js");
 
@@ -16,6 +18,8 @@ const router = express.Router();
 router.post("/add_product", addProducts);
 router.delete("/remove_product/:id/:productId", removeProducts);
 router.put("/update_product/:id/:productId", updateProducts);
+router.post("/addimage", addImage);
+router.post("/deleteimage", deleteImage);
 
 // Get Request routes
 router.get("/get_all_Products", getProducts);
