97d58a198a09cc6a1d15913ffbcc990c64ed149c
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-10-23 01:16:09 +0530

    Update payment.js

--- routes/payment.js
+++ routes/payment.js
@@ -5,7 +5,7 @@ const { checkOut, verifyPayment, getKey } = require("../controllers/payment");
 const router = express.Router();
 
 router.post("/checkout", checkOut);
-router.post("/verifyPayment", verifyPayment);
+router.post("/verifyPayment/:orderId", verifyPayment);
 router.get("/getKey", getKey);
 
 module.exports = router;
