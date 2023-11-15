e2056f55eb487d8f99740b70900467275e291310
Chandan Rajput <113819484+Chandan17801@users.noreply.github.com>
2023-11-15 18:13:13 +0530

    Email

--- controllers/user.js
+++ controllers/user.js
@@ -197,15 +197,34 @@ exports.verify = async (req, res) => {
 exports.createEmployeeAccount = async (req, res) => {
   try {
     const { name, email, phone, password, adminType } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
     const otp = generateOTP();
     const employee = await Users.create({
       name,
       email,
       phone,
       phone_OTP: otp.toString(),
-      password,
+      password: hashedPassword,
       adminType,
     });
+
+    const transporter = nodemailer.createTransport({
+      service: "GMAIL",
+      auth: {
+        user: process.env.USER_E_MAIL,
+        pass: process.env.USER_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.USER_E_MAIL,
+      to: email,
+      subject: "Welcome to Our Company",
+      text: `Dear ${name},\n\nWelcome to our company! Your account has been successfully created. Your password is as: ${password}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+
     res.status(201).json({ message: "Account created", user: employee });
   } catch (error) {
     console.log(error);
