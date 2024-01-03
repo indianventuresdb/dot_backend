0065dd012cf61b0a14079c3b737053b89b61f32f
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2024-01-03 13:46:42 +0530

    sharp removed

--- controllers/files.js
+++ controllers/files.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const Fs = require("fs");
-const fs = require("fs").promises;
-const sharp = require("sharp");
+// const fs = require("fs").promises;
+// const sharp = require("sharp");
 
 
 exports.downloadImageController = async (req, res) => {
@@ -25,40 +25,40 @@ exports.downloadImageController = async (req, res) => {
 
 // Use fs.promises for consistent promise-based API
 
-exports.imageOptimizer = async (req, res) => {
-    const fileName = req.params.fileName;
-    const width = parseInt(req.query.width) || 300; // Default width is 300
-    const height = parseInt(req.query.height) || 300; // Default height is 300
+// exports.imageOptimizer = async (req, res) => {
+//     const fileName = req.params.fileName;
+//     const width = parseInt(req.query.width) || 300; // Default width is 300
+//     const height = parseInt(req.query.height) || 300; // Default height is 300
 
-    // Construct the file paths
-    const originalFilePath = path.join(__dirname, '../uploads', fileName);
-    const optimizedDirectory = path.join(__dirname, '../uploads', 'optimized');
-    const optimizedFilePath = path.join(optimizedDirectory, `${fileName}_${width}x${height}.jpg`);
+//     // Construct the file paths
+//     const originalFilePath = path.join(__dirname, '../uploads', fileName);
+//     const optimizedDirectory = path.join(__dirname, '../uploads', 'optimized');
+//     const optimizedFilePath = path.join(optimizedDirectory, `${fileName}_${width}x${height}.jpg`);
 
-    console.log("original : ", originalFilePath);
-    console.log("optimized : ", optimizedFilePath);
+//     console.log("original : ", originalFilePath);
+//     console.log("optimized : ", optimizedFilePath);
 
-    try {
+//     try {
 
-        await fs.access(originalFilePath);
+//         await fs.access(originalFilePath);
 
-        await fs.mkdir(optimizedDirectory, { recursive: true });
-
-
-        const optimizedFileExists = await fs.access(optimizedFilePath).then(() => true).catch(() => false);
-
-        if (!optimizedFileExists) {
-
-            await sharp(originalFilePath)
-                .resize({ width, height })
-                .toFile(optimizedFilePath);
-        }
+//         await fs.mkdir(optimizedDirectory, { recursive: true });
 
 
-        res.sendFile(optimizedFilePath);
-    } catch (err) {
+//         const optimizedFileExists = await fs.access(optimizedFilePath).then(() => true).catch(() => false);
 
-        console.log(err);
-        res.status(404).send('File not found');
-    }
-};
+//         if (!optimizedFileExists) {
+
+//             await sharp(originalFilePath)
+//                 .resize({ width, height })
+//                 .toFile(optimizedFilePath);
+//         }
+
+
+//         res.sendFile(optimizedFilePath);
+//     } catch (err) {
+
+//         console.log(err);
+//         res.status(404).send('File not found');
+//     }
+// };
--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { downloadImageController, imageOptimizer } = require("../controllers/files.js");
+const { downloadImageController } = require("../controllers/files.js");
 
 const router = express.Router();
 
 router.get("/uploads/:fileName", downloadImageController);
-router.get("/opt/:fileName", imageOptimizer);
+// router.get("/opt/:fileName", imageOptimizer);
 
 module.exports = router;
