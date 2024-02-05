aaf33a841a6b9fbfd1d2f3e82dcc4181e64ac338
surendraSinghKamboj <surendra.singh.kamboj@hotmail.com>
2024-02-05 11:36:52 +0530

    image sharped

--- routes/files.js
+++ routes/files.js
@@ -3,7 +3,7 @@ const { downloadImageController } = require("../controllers/files.js");
 
 const router = express.Router();
 
+// router.get("/uploads/:fileName", downloadImageController);
 router.get("/uploads/:fileName", downloadImageController);
-// router.get("/opt/:fileName", imageOptimizer);
 
 module.exports = router;
