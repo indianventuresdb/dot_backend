3e65d4f7bc7da582f2483dd2f9e80ac10faedde2
Surendra Singh Kamboj <surendra.singh.kamboj@hotmail.com>
2024-04-27 16:58:17 +0530

    log fixed

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,6 +1,13 @@
 exports.logger = (req, res, next) => {
+  const timestamp = formatDate(new Date());
   res.on("finish", function() {
-      console.log(req.method, decodeURI(req.url), res.statusCode, res.statusMessage);
+    console.log(`${timestamp} || ${req.method} ${decodeURI(req.url)} ${res.statusCode} ${res.statusMessage}`);
   });
   next();
 };
+
+function formatDate(date) {
+  const pad = num => (num < 10 ? '0' : '') + num;
+  const formattedDate = `${pad(date.getDate())}/${pad(date.getMonth() + 1)}/${date.getFullYear()} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}:${pad(date.getMilliseconds())}`;
+  return formattedDate;
+}
