5933d6d2c3de46cf70cfbe84ca9797d195aeee4f
Surendra Singh Kamboj <surendra.singh.kamboj@hotmail.com>
2024-05-08 16:59:40 +0530

    logger updated

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,13 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
 exports.logger = (req, res, next) => {
   const timestamp = formatDate(new Date());
-  res.on("finish", function() {
-    console.log(`${timestamp} || ${req.method} ${decodeURI(req.url)} ${res.statusCode} ${res.statusMessage}`);
-  });
+  const logMessage = `${timestamp},${req.method},${decodeURI(req.url)},${res.statusCode},${res.statusMessage}\n`;
+
+  // Writing logs to a CSV file
+  const logFilePath = getLogFilePath();
+  if (!fs.existsSync(logFilePath)) {
+    // Write header if file doesn't exist
+    const header = 'Timestamp,Method,URL,StatusCode,StatusMessage\n';
+    fs.writeFileSync(logFilePath, header);
+  }
+  fs.appendFileSync(logFilePath, logMessage);
+
+  // Log errors separately
+  if (res.statusCode >= 400) {
+    const errorLogFilePath = getErrorLogFilePath();
+    if (!fs.existsSync(errorLogFilePath)) {
+      // Write header if file doesn't exist
+      const header = 'Timestamp,Method,URL,StatusCode,StatusMessage\n';
+      fs.writeFileSync(errorLogFilePath, header);
+    }
+    fs.appendFileSync(errorLogFilePath, logMessage);
+  }
+
   next();
 };
 
 function formatDate(date) {
   const pad = num => (num < 10 ? '0' : '') + num;
-  const formattedDate = `${pad(date.getDate())}/${pad(date.getMonth() + 1)}/${date.getFullYear()} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}:${pad(date.getMilliseconds())}`;
+  const formattedDate = `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
   return formattedDate;
+}
+
+// Function to get the log file path
+function getLogFilePath() {
+  const logsDirectory = path.join(__dirname, '../logs');
+  if (!fs.existsSync(logsDirectory)) {
+    fs.mkdirSync(logsDirectory);
+  }
+  const logFilePath = path.join(logsDirectory, 'logs.csv');
+  return logFilePath;
+}
+
+// Function to get the error log file path
+function getErrorLogFilePath() {
+  const logsDirectory = path.join(__dirname, 'logs');
+  if (!fs.existsSync(logsDirectory)) {
+    fs.mkdirSync(logsDirectory);
+  }
+  const errorLogFilePath = path.join(logsDirectory, 'error_logs.csv');
+  return errorLogFilePath;
 }
